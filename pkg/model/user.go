c2062254141f61a23a0c4c6c7bb2f694957dc204
AQian <970506352@qq.com>
2023-12-14 11:44:19 +0800

    feat: 实现用户注册时密码加密

--- pkg/model/user.go
+++ pkg/model/user.go
@@ -1,6 +1,9 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
@@ -8,4 +11,13 @@ type User struct {
 	Email    string
 	Password string
 	Nickname string
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	password, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(password)
+	return nil
 }
